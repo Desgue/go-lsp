a57ba794075a4a8f59e22a3b486685fd70a17877
Desgue <guedesgomero@gmail.com>
2024-03-21 13:48:41 +0000

    add log and lsp message types

--- main.go
+++ main.go
@@ -2,16 +2,38 @@ package main
 
 import (
 	"bufio"
+	"log"
 	"os"
+
+	"github.com/Desgue/go-lsp/rpc"
 )
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
-
+	//scanner.Split(rpc.Split)
+	logger := getLogger("C:/Users/guede/code/go-lsp/lsp.log")
+	logger.Println("Starting LSP")
 	for scanner.Scan() {
-		print("\nmesage: ", scanner.Text(), "\n")
+		msg := scanner.Bytes()
+		method, contents, err := rpc.DecodeMessage(msg)
+		if err != nil {
+			logger.Printf("Error: %s", err)
+			continue
+		}
+
+		handleMessage(logger, method, contents)
 	}
 
 }
 
-//type SplitFunc func(data []byte, atEOF bool) (advance int, token []byte, err error)
+func handleMessage(logger *log.Logger, method string, contetns []byte) {
+	logger.Printf("Received message with method: %s", method)
+}
+
+func getLogger(filename string) *log.Logger {
+	logFile, err := os.OpenFile(filename, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0666)
+	if err != nil {
+		panic("Failed to open log file")
+	}
+	return log.New(logFile, "[golsp]", log.Ldate|log.Ltime|log.Lshortfile)
+}
